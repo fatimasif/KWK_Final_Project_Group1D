0a46f387291ec1f0d326b30ad8cc630f04dac0a0
Fatima Asif <fatimasif2004@gmail.com>
2022-07-20 11:37:37 -0400

    I added all the view controllers and their respective swift coding screens. Additionally, I changed the background colors of each screen to match the room and added buttons as segues for each screen.

---
+++ KWK_Final_Project_Group1D/closetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  closetViewController.swift
+//  KWK_Final_Project_Group1D
+//
+//  Created by Fatima Asif on 7/20/22.
+//
+
+import UIKit
+
+class closetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
